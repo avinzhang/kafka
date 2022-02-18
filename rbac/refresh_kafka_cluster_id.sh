982f7dc2f06c8b15809be58c9b3b4a871dbfe028
Avin Zhang <avin@aMBP.local>
2022-02-18 10:18:49 +1000

    Updated rbac refresh script

--- rbac/refresh_kafka_cluster_id.sh
+++ rbac/refresh_kafka_cluster_id.sh
@@ -3,7 +3,7 @@
 OUTPUT=$(
 expect <<END
   log_user 1
-  spawn confluent login --url https://localhost:8090 --ca-cert-path ./secrets/ca.crt
+  spawn confluent login --url https://localhost:1090 --ca-cert-path ./secrets/ca.crt
   expect "Username: "
   send "superUser\r";
   expect "Password: "
@@ -14,12 +14,7 @@ END
 )
 
 echo "$OUTPUT"
-
-  if [[ ! "$OUTPUT" =~ "Logged in as" ]]; then
-    echo "Failed to log into MDS.  Please check all parameters and run again"
-    exit 1
-  fi
 echo
-KAFKA_CLUSTER_ID=$(curl -sk  https://localhost:8090/v1/metadata/id  | grep id |jq -r ".id")
+KAFKA_CLUSTER_ID=$(curl -sk  https://localhost:1090/v1/metadata/id  | grep id |jq -r ".id")
 echo "Kafka cluster id is $KAFKA_CLUSTER_ID"
 
