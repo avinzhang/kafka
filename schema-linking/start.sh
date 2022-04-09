cc7d2bff314e9f9904802b3790b217295b7cb2a7
Avin Zhang <avin@example.com>
2022-04-09 20:02:25 +1000

    Schema-linking: updated for rancher

--- schema-linking/start.sh
+++ schema-linking/start.sh
@@ -35,6 +35,19 @@ done
 echo
 echo
 
+STARTED=false
+while [ $STARTED == false ]
+do
+    docker-compose logs schemaregistry1 | grep "Server started" &> /dev/null
+    if [ $? -eq 0 ]; then
+      STARTED=true
+      echo "SR1 is started and ready"
+    else
+      echo "Waiting for SR1 to start..."
+    fi
+    sleep 5
+done
+
 echo ">>Register schemas on schemaregistry1"
 curl -s -X POST -H "Content-Type: application/json" --data '{"schema": "{\"type\":\"record\",\"name\":\"Users\",\"fields\":[{\"name\":\"Name\",\"type\":\"string\"},{\"name\":\"Age\",\"type\":\"int\"},{\"name\":\"Phone\",\"type\":\"int\"}]}"}' http://localhost:1081/subjects/:.people:users/versions
 #curl -s http://localhost:1081/subjects/:.people:users/versions/1
@@ -73,22 +86,23 @@ curl --silent -X GET http://localhost:1081/subjects?subjectPrefix=":*:" | jq
 
 
 echo ">>Create exporter on source schema registry"
-docker-compose exec schemaregistry1 bash -c 'cat << EOF > /tmp/config.txt
+cat << EOF > /tmp/config.txt
 schema.registry.url=http://schemaregistry2:2081
-EOF'
-docker-compose exec schemaregistry1 schema-exporter --create --name myschemalink --context-name myschemalink --subjects ":*:" --schema.registry.url http://schemaregistry1:1081/ --config-file /tmp/config.txt
+EOF
+schema-exporter --create --name myschemalink --context-type CUSTOM --context-name myschemalink --subjects ":*:" --schema.registry.url http://localhost:1081/ --config-file /tmp/config.txt
 
 echo ">>List schema exporter"
-docker-compose exec schemaregistry1 schema-exporter --list --schema.registry.url http://schemaregistry1:1081
+schema-exporter --list --schema.registry.url http://localhost:1081
 
 echo
 echo ">>Describe schema exporter"
-docker-compose exec schemaregistry1 schema-exporter --describe --name myschemalink --schema.registry.url http://schemaregistry1:1081
+schema-exporter --describe --name myschemalink --schema.registry.url http://localhost:1081
 
 echo
 echo ">>Get status of exporter"
-docker-compose exec schemaregistry1 schema-exporter --get-status --name myschemalink --schema.registry.url http://schemaregistry1:1081
+schema-exporter --get-status --name myschemalink --schema.registry.url http://localhost:1081
 echo
+sleep 5
 echo
 echo ">>Check schemas on schemaregistry2"
 curl --silent -X GET http://localhost:2081/subjects?subjectPrefix=":*:" | jq
