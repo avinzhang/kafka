79f8fd8ff85a2f66f6dfc7d89cbc15281d0201ea
Avin Zhang <avin@example.com>
2022-04-18 18:01:07 +1000

    ksqldb materialized: updated for arm

--- ksqldb/materialized_cache/start.sh
+++ ksqldb/materialized_cache/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export TAG=7.0.1
+export TAG=7.1.0.arm64
 
 echo "----Download  connector-----------"
 mkdir -p ./jar/debezium
@@ -10,14 +10,14 @@ echo "Done"
 echo
 echo
 echo "----Start everything up with version $TAG------------"
-docker-compose up -d --build --no-deps zookeeper kafka schemaregistry ksqldb-server mysql #&>/dev/null
+docker compose up -d --build --no-deps zookeeper kafka schemaregistry ksqldb-server mysql #&>/dev/null
 echo
 
 echo
 ksql_ready=false
 while [ $ksql_ready == false ]
 do
-    docker-compose logs ksqldb-server|grep "Server up and running" &> /dev/null
+    docker compose logs ksqldb-server|grep "Server up and running" &> /dev/null
     if [ $? -eq 0 ]; then
       ksql_ready=true
       echo "*** ksqldb is ready ****"
@@ -27,14 +27,14 @@ do
     sleep 5
 done
 exit
-docker-compose exec mysql bash -c "mysql -uroot -proot <<EOF
+docker compose exec mysql bash -c "mysql -uroot -proot <<EOF
 GRANT ALL PRIVILEGES ON *.* TO 'example-user' WITH GRANT OPTION;
 ALTER USER 'example-user'@'%' IDENTIFIED WITH mysql_native_password BY 'example-pw';
 FLUSH PRIVILEGES;
 EOF"
 
 echo "----Create connector in ksqldb----------------"
-docker-compose exec ksqldb-server bash -c "ksql http://ksqldb-server:8088 <<EOF
+docker compose exec ksqldb-server bash -c "ksql http://ksqldb-server:8088 <<EOF
 SET 'auto.offset.reset'='earliest';
 CREATE SOURCE CONNECTOR calls_reader WITH (
     'connector.class' = 'io.debezium.connector.mysql.MySqlConnector',
@@ -58,7 +58,7 @@ sleep 5
 echo 
 echo 
 echo "Create materialized views"
-docker-compose exec ksqldb-server bash -c "ksql http://ksqldb-server:8088 <<EOF
+docker compose exec ksqldb-server bash -c "ksql http://ksqldb-server:8088 <<EOF
 SET 'auto.offset.reset'='earliest';
 SHOW TOPICS;
 
@@ -91,7 +91,7 @@ echo
 echo
 echo "Query materialized views"
 sleep 3
-docker-compose exec ksqldb-server bash -c "ksql http://ksqldb-server:8088 <<EOF
+docker compose exec ksqldb-server bash -c "ksql http://ksqldb-server:8088 <<EOF
 SET 'auto.offset.reset'='earliest';
 SELECT name, distinct_reasons, last_reason FROM support_view WHERE name = 'derek';
 
@@ -104,7 +104,7 @@ EOF"
 
 echo 
 echo "---Create datagen connector"
-docker-compose exec ksqldb-server bash -c "ksql http://ksqldb-server:8088 <<EOF
+docker compose exec ksqldb-server bash -c "ksql http://ksqldb-server:8088 <<EOF
 SET 'auto.offset.reset'='earliest';
 CREATE SOURCE CONNECTOR datagenusers WITH (
     'connector.class' = 'io.confluent.kafka.connect.datagen.DatagenConnector',
