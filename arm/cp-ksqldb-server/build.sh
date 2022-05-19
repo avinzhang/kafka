ca7703d96ce71f95348a5d8f7164acdc289a3c07
Avin Zhang <avin@example.com>
2022-05-19 16:55:10 +1000

    Updated arm ksqldb server build script

--- arm/cp-ksqldb-server/build.sh
+++ arm/cp-ksqldb-server/build.sh
@@ -2,13 +2,16 @@
 
 VERSION=7.1.1
 
+
 mkdir artifacts
-rsync -a /Users/avin/confluentinc/cpe/confluent-${VERSION}/share/java/ksqldb/ artifacts/
+rsync -a ~/confluentinc/cpe/confluent-${VERSION}/share/java/ksqldb/ artifacts/
+#copy monitoring interceptor
+cp ~/confluentinc/cpe/confluent-${VERSION}/share/java/monitoring-interceptors/* artifacts/
 
 cd /Users/avin/confluentinc/kafka/src/ksql
 git checkout ${VERSION}-post
 
 mkdir ksqldb-console-scripts
-rsync -a /Users/avin/confluentinc/kafka/src/ksql/bin/ ksqldb-console-scripts/
+rsync -a ~/confluentinc/kafka/src/ksql/bin/ ksqldb-console-scripts/
 
 #docker build . -t confluentinc/cp-ksqldb-server:7.1.1.arm64
