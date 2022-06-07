8af9d214a0d73330d59ef740ecc6dd4fbb14e0df
Avin Zhang <avin@example.com>
2022-06-07 14:53:50 +1000

    Updated destroy script for confluent cloud

--- ccloud/terraform/destroy.sh
+++ ccloud/terraform/destroy.sh
@@ -1,9 +1,12 @@
 echo ">>Export api key as environment variables"
-export CONFLUENT_CLOUD_API_KEY="`cat /tmp/cloud_api_key | jq -r .key`"
-export CONFLUENT_CLOUD_API_SECRET="`cat /tmp/cloud_api_key | jq -r .secret`"
+#export CONFLUENT_CLOUD_API_KEY="`cat /tmp/cloud_api_key | jq -r .key`"
+#export CONFLUENT_CLOUD_API_SECRET="`cat /tmp/cloud_api_key | jq -r .secret`"
 
 terraform destroy --auto-approve
 
-confluent api-key delete $CONFLUENT_CLOUD_API_KEY
+for CONFLUENT_CLOUD_API_KEY in `confluent api-key list  -ojson | jq -r '.[]|select(.owner_email|startswith("avin+cops")).key'`
+  do 
+    confluent api-key delete $CONFLUENT_CLOUD_API_KEY
+done
 
 rm terraform.tfstate* tfplan
