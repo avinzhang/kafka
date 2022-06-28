b4ccfeb2a5a4ae376c4d0fe87446f30af87529c5
Avin Zhang <avin@example.com>
2022-06-28 21:10:25 +1000

    Updated multi plaintext updated startup

--- plaintext/multi-brokers/start.sh
+++ plaintext/multi-brokers/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export TAG=7.1.1.arm64
+export TAG=7.1.2.arm64
 
 echo "----------Start zookeeper and broker -------------"
 docker compose up -d --build --no-deps zookeeper1 zookeeper2 zookeeper3 
