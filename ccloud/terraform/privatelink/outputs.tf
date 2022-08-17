ff12d8897c5dfac2a8f4d0c38b1869cf22210007
Avin Zhang <avin@example.com>
2022-08-17 17:38:18 +1000

    cloud terraform privatelink: add app-manager api key to output

--- ccloud/terraform/privatelink/outputs.tf
+++ ccloud/terraform/privatelink/outputs.tf
@@ -2,5 +2,14 @@ output "bootstrap" {
   value = "${local.bootstrap}"
 }
 
+#output "app-manager-api-key" {
+#  value = "${confluent_api_key.app-manager-kafka-api-key.id}"
+#}
+#
+#output "app-manager-api-secret" {
+#  value = "${confluent_api_key.app-manager-kafka-api-key.secret}"
+#  sensitive = true
+#}
+
 
 
