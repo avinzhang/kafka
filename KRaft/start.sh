bf5f483ab0e1c8324a7c90a19e489784883cdc55
Avin Zhang <avin@example.com>
2022-10-04 11:44:46 +1000

    Updated KRaft setup

--- KRaft/start.sh
+++ KRaft/start.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 echo "Setup metadata.properties for broker logs"
-mkdir -p logs/kafka logs/kafka1 logs/kafka2
+mkdir -p logs/kafka1 logs/kafka3 logs/kafka2
 CLUSTER_ID=`kafka-storage random-uuid`
-cat << EOF > ./logs/kafka/meta.properties
-cluster.id=$CLUSTER_ID
-version=1
-node.id=0
-EOF
-
-
 cat << EOF > ./logs/kafka1/meta.properties
 cluster.id=$CLUSTER_ID
 version=1
@@ -22,6 +15,13 @@ cluster.id=$CLUSTER_ID
 version=1
 node.id=2
 EOF
+
+
+cat << EOF > ./logs/kafka3/meta.properties
+cluster.id=$CLUSTER_ID
+version=1
+node.id=3
+EOF
 echo
 echo "Start brokers in KRaft mode"
-docker-compose up -d kafka kafka1 kafka2
+docker-compose up -d kafka1 kafka2 kafka3
